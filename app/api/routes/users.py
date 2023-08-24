c166f85ca575479e18551e44b41518154eae41c3
laVida666 <max0211velichko@gmail.com>
2023-08-24 22:48:27 +0300

    Добавлена проверка токена.

--- app/api/routes/users.py
+++ app/api/routes/users.py
@@ -8,7 +8,7 @@ from app.api.errors import USER_ALREADY_EXISTS, PASSWORD_INCORRECT, USER_NOT_FOU
 from app.config import Config
 from app.db import get_repository
 from app.db.repositories.user import UserRepository
-from app.models.users import RegisterRequest, LoginRequest
+from app.models.users import RegisterRequest, LoginRequest, CheckRequest
 
 config = Config()
 
@@ -53,7 +53,11 @@ async def login(
         if user_exists.password == body.password:
             payload = {
                 "mail": body.mail,
+                "name": user_exists.name,
+                "surname": user_exists.surname,
+                "phone": user_exists.phone,
                 "password": body.password,
+                "role": user_exists.role,
                 "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
             }
             token = jwt.encode(payload, config.app.secret_key, algorithm="HS256")
@@ -64,4 +68,29 @@ async def login(
         return JSONResponse({"error": USER_NOT_FOUND}, status_code=404)
 
 
-
+@router.post(
+    path="/check",
+    description="Проверка токена"
+)
+async def check_token(
+        body: CheckRequest,
+        user_repo: UserRepository = Depends(get_repository(UserRepository))
+):
+    if body != "":
+        user_check = jwt.decode(body.token, config.app.secret_key, algorithms="HS256")
+        user = await user_repo.get_user_by_email(user_check.get('mail'))
+        if not user:
+            return JSONResponse({"error": USER_NOT_FOUND}, status_code=404)
+        else:
+            if user.password == user_check.get('password'):
+                return JSONResponse({"access_token": jwt.encode({
+                    "mail": user.mail,
+                    "name": user.name,
+                    "surname": user.surname,
+                    "phone": user.phone,
+                    "password": user.password,
+                    "role": user.role,
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(hours=1),
+                }, config.app.secret_key, algorithm="HS256")})
+            else:
+                return JSONResponse({"error": PASSWORD_INCORRECT}, status_code=500)
