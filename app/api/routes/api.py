5ad532970aa9ce0c83cf2354dceac9423ccb40ac
Mikhail Stepanov <mstepanov@ptsecurity.com>
2023-08-25 13:17:50 +0300

    Currency router import

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
 
-from app.api.routes import users, wallets, bank
+from app.api.routes import users, wallets, bank, currencies
 
 router = APIRouter()
 router.include_router(users.router)
 router.include_router(wallets.router)
 router.include_router(bank.router)
+router.include_router(currencies.router)
